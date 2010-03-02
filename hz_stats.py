3eada4f01a1977e1bd5e30504f2ab5cb6fd299c5
m5 <mfivecoa@purdue.edu>
2010-03-02 04:08:23 -0500

    made things nicer for execution

--- hz_stats.py
+++ hz_stats.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 from collections import defaultdict
 from hz_parser import get_entries
 from hz_counters import update_counts, counters
@@ -5,6 +7,17 @@ from hz_funnels import funnels
 from mako.template import Template
 import datetime
 import sys
+import glob
+import os
+
+usage = """%s <log file path> <out file path>
+Parses the most recent log file in `in file path`,
+and outputs statistics under the current date in `out file path`""" % sys.argv[0]
+
+def helpful_exit():
+    print usage
+    sys.exit(0)
+
 
 def get_stats(f):
     counts = defaultdict(lambda:0)
@@ -23,7 +36,7 @@ def get_stats(f):
 def write_stats(counts, funnels, today, path='./'):
     stats_temp = Template(filename="stats.mako")
     today_pretty = today.strftime("%A, %B %d, %Y")
-    today_filename = today.strftime("%Y-%m-%d")+".html"
+    today_filename = today.strftime("%Y-%m-%d.html")
     html = stats_temp.render(counts=counts,
                              funnels=funnels,
                              today=today_pretty)
@@ -34,15 +47,26 @@ def write_stats(counts, funnels, today, path='./'):
 
 if __name__ == "__main__":
     try:
-        filename = sys.argv[1]
+        in_path = sys.argv[1]
     except IndexError:
-        filename = "data/heyzap-example-log"
-
+        print usage
+        sys.exit(1)
+        
     try:
-        path = sys.argv[2]
+        out_path = sys.argv[2]
     except IndexError:
-        path = "./"
-    
+        print usage
+        sys.exit(1)
+
+    dir_contents = glob.glob(in_path+'/*')
+    try:
+        filename = sorted(dir_contents,key=lambda f: os.path.getctime(f))[0]
+    except IndexError:
+        print usage
+        sys.exit
+        
     f = open(filename)
     counts, funnels, uniques = get_stats(f)
-    write_stats(counts, funnels, datetime.date.today(), path)
+    write_stats(counts, funnels, datetime.date.today(), out_path)
+
+
